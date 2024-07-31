882bb6ea54b1a295a41e0c5670c8d40bbe2a55a3
M.Sulaeman-PPLG2 <113089132+MSulaeman@users.noreply.github.com>
2024-07-31 21:12:37 +0700

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "koneksi.php";
-$nama = $_GET['nama'];
-$sql = "DELETE from siswa where nama ='$nama'";
-$query = mysqli_query($conn, $sql);
-if (!$query) {
-die(mysqli_error($conn));   
-}
-header("Location: index.php");
