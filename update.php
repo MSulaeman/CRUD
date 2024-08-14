d1ad55335afc3dd019e611d3108dcdb496d2b5d4
M.Sulaeman-PPLG2 <113089132+MSulaeman@users.noreply.github.com>
2024-08-14 09:36:50 +0700

    update.php

--- update.php
+++ update.php
@@ -1,11 +1,11 @@
 <?php
 include 'koneksi.php';
+
+$id   = $_POST['id'];
 $nama = $_POST['nama'];
 $umur = $_POST['umur'];
 
-$sql = "UPDATE siswa set umur='$umur' where nama='$nama'";
+$sql = "UPDATE siswa SET nama='$nama',umur='$umur' WHERE id='$id'";
 $query = mysqli_query($conn, $sql);
-if (!$query) {
-die(mysqli_error($conn));
-}
+
 header("Location: index.php");
